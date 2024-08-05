2874994b3862b5ef7fe6695035ee137d949cc46a
pcoloigner <philippe.coloigner@gmail.com>
2024-08-05 11:54:23 +0200

    classe application

---
+++ infrastructure/backend/comptavolleybackend/src/test/java/org/pcomeziantou/comptavolley/comptavolleybackend/ComptavolleybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pcomeziantou.comptavolley.comptavolleybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComptavolleybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
